b1e1d4daa6bd0decd3cce096cacef87737222325
Sathya <rsathiradha666@gmail.com>
2026-08-31 09:08:09 -0700

    my index page with head and title

---
+++ Two.java
@@ -0,0 +1,10 @@
+class Two{
+static void add(int a,int b){
+System.out.println(a+b);
+
+}
+public static void main(Strin[] ar){
+add(12,23);
+Two.add(1,2);
+
+}
---
+++ one2.java
@@ -0,0 +1,8 @@
+class one{
+public static void main(String[] args){
+
+System.out.println("welcome to java World");
+System.out.println("Java Full Stack is a primary skill");
+}
+}
+
